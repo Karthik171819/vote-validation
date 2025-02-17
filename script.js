d029396f58bebee7fdf01f3bd4dd4d5c4a2aff16
Karthik171819 <karthikkumar092004@gmail.com>
2025-02-17 13:44:36 +0530

    files added

---
+++ script.js
@@ -0,0 +1,12 @@
+function vot()
+{
+    var name=document.getElementById("name").value;
+    var age=document.getElementById("age").value;
+    if(age>=18){
+        alert("eligible to vote");
+}
+        else{
+            alert("not eligible to vote");
+        }
+        
+}
